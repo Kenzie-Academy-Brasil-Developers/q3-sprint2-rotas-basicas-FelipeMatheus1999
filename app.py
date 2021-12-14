9a1bcf0259f9023184faf31d38bb75228ec7b25c
FelipeMatheus1999 <felipe_cavalcante7939@hotmail.com>
2021-12-14 14:39:10 -0300

    feat: Added route "/" and "/current_datetime"

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-# Código do dev aqui
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def get():
+    return {"data": "Hello Flask!"}
+
+@app.route("/current_datetime", methods=["GET"])
+def current_datetime():
+    date = int(datetime.now().strftime("%H"))
+    current_datetime = datetime.now().strftime("%H/%m/%Y %X %p")
+    message = ""
+
+    if date >= 6 and date < 12:
+        message = "Bom dia!"
+    
+    elif date >= 12 and date < 18:
+        message = "Boa tarde!"
+    
+    else:
+        message = "Boa noite!"
+    
+    return {"current_datetime": current_datetime, "message": message}
